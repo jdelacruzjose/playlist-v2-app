b6d5ddd515fa0528041d099cf5c1b5a9197456bc
Jose De la Cruz <jdelacruzjose@gmail.com>
2020-06-17 20:10:20 -0400

    Mobile Navbar Setup

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import BackDrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, openTheDrawer] = useState(false);
+
+  const openDrawer = () => {
+    openTheDrawer(true);
+  };
+
+  const closeDrawer = () => {
+    openTheDrawer(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <BackDrop onClick={closeDrawer} />}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
